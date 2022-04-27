cf2ad18d068a8caa80608db10df9dbbf07cca03b
Gilbert Kwan <gkwan@ca.ibm.com>
2022-04-27 11:49:41 -0400

    add startup check

---
+++ simple-rest-app/src/main/java/com/demo/rest/health/RestStartUpCheck.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+
+package com.demo.rest.health;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.health.Startup;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+@Startup
+@ApplicationScoped
+public class RestReadinessCheck implements HealthCheck {
+
+  @Override
+  public HealthCheckResponse call() {
+
+    // if not ready logic
+    // return HealthCheckResponse.down(this.getClass().getSimpleName());
+    return HealthCheckResponse
+            .up(this.getClass().getSimpleName());
+  }
+}
