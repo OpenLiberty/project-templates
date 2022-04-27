b67d2d720067ed65491681b148ddc56735058d3c
Gilbert Kwan <gkwan@ca.ibm.com>
2022-04-27 11:09:24 -0400

    update versions

--- simple-rest-app/src/main/java/com/demo/rest/health/RestReadinessCheck.java
+++ simple-rest-app/src/main/java/com/demo/rest/health/RestReadinessCheck.java
@@ -1,17 +1,17 @@
 /*******************************************************************************
- * Copyright (c) 2020 IBM Corporation and others.
+ * Copyright (c) 2020, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
+ *     IBM Corporation - Initial implementation
  *******************************************************************************/
 
 package com.demo.rest.health;
 
-import javax.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.ApplicationScoped;
 
 import org.eclipse.microprofile.health.Readiness;
 import org.eclipse.microprofile.health.HealthCheck;
--- simple-rest-app/src/test/java/com/demo/rest/test/RestEndpointIT.java
+++ simple-rest-app/src/test/java/com/demo/rest/test/RestEndpointIT.java
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
+ *     IBM Corporation - Initial implementation
  *******************************************************************************/
 
 package com.demo.rest.test;
